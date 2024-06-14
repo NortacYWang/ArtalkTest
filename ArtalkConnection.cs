d05d9d1c9284e34b65c18109b34922ff2947261c
YipanWang <YipanWang@OTT-ENG-YIPANW>
2024-06-14 15:30:48 -0400

    Add project files.

---
+++ ArtalkConnection.cs
@@ -0,0 +1,187 @@
+﻿using Artalk.Xmpp;
+using Artalk.Xmpp.Client;
+using Artalk.Xmpp.Extensions;
+using Artalk.Xmpp.Im;
+
+namespace ArtalkTest
+{
+    public class ArtalkConnection
+    {
+
+        private ArtalkXmppClient _client;
+
+        public ArtalkConnection(string hostname, string username, string password)
+        {
+            _client = new ArtalkXmppClient(hostname, username, password);
+            
+            _client.FileTransferSettings.ForceInBandBytestreams = true;
+            _client.FileTransferSettings.UseUPnP = true;
+
+            _client.Message += OnNewMessage;
+            _client.SubscriptionRequest = OnSubscriptionRequest;
+            _client.SubscriptionApproved += OnSubscriptionApproved;
+            _client.SubscriptionRefused += OnSubscriptionRefused;
+            _client.FileTransferRequest = OnFileTransferRequest;
+            _client.FileTransferProgress += OnFileTransferProgress;
+            _client.FileTransferAborted += onFileTransferAborted;
+        }
+
+        public bool Connect()
+        {
+            try
+            {
+                _client.Connect();
+                return true;
+
+            } catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+
+            }
+        }
+
+        public List<RosterItem> getContacts()
+        {
+            Roster roaster = _client.GetRoster();
+            return roaster.ToList();
+
+        }
+
+        public Jid getOwnJid()
+        {
+            return _client.Jid;
+        }
+
+        public List<string> getFeatures(Jid id)
+        {
+            return _client.GetFeatures(id).ToList();
+        }
+
+        public bool SendMessage(Jid recipientId, string message)
+        {
+            try
+            {
+                _client.SendMessage(recipientId, message, null, null, MessageType.Chat);
+                return true;
+            } catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+
+            }
+            
+        }
+
+        // to set status
+        public bool SetStatus(Availability availability, string message)
+        {
+            try
+            {
+                _client.SetStatus(availability, message);
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+
+        }
+
+        // to publish tune information to other contacts.
+        public bool SetTune(TuneInformation tune)
+        {
+            try
+            {
+                _client.SetTune(tune);
+                return true;
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+            
+        }
+
+        //  publish the user's current activity.
+        public bool SetActivity(GeneralActivity activity, SpecificActivity special)
+        {
+            try
+            {
+                _client.SetActivity(activity, special);
+           
+                return true;
+
+            } catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+
+            }
+            
+        }
+
+        public static void FileTransferCallback(bool hasSent, FileTransfer file)
+        {
+            Console.WriteLine("has sent file >" + hasSent);
+
+        }
+
+        public void InitialFileTransfer(Jid to, string path)
+        {
+            _client.InitiateFileTransfer(to, path, null, FileTransferCallback);
+        }
+
+        public void OnNewMessage(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine("Message from <" + e.Jid + ">: " + e.Message.Body);
+        }
+
+        public void OnFileTransferProgress(object sender, FileTransferProgressEventArgs e)
+        {
+            Console.WriteLine("File transfer propress <" + e.Transfer.Name + ">: " +e.Transfer.SessionId +  e.Transfer.Transferred );
+        }
+
+        public void onFileTransferAborted(object sender, FileTransferAbortedEventArgs e)
+        {
+            Console.WriteLine("File transfer aborted" + e.Transfer.Name + ">: " + e.Transfer.Transferred);
+
+        }
+
+        public void OnSubscriptionApproved(object sender, SubscriptionApprovedEventArgs e )
+        {
+            Console.WriteLine("Subscription Approved from <" + e.Jid + ">: ");
+
+        }
+        public void OnSubscriptionRefused(object sender, SubscriptionRefusedEventArgs e)
+        {
+            Console.WriteLine("Subscription refused from <" + e.Jid + ">: ");
+
+        }
+
+        static bool OnSubscriptionRequest(Jid from)
+        {
+            Console.WriteLine(from + " wants to subscribe to your presence.");
+            Console.Write("Type Y to approve the request, or any other key to refuse it: ");
+
+            // Return true to approve the request, or false to refuse it.
+            return Console.ReadLine().ToLowerInvariant() == "y";
+        }
+
+        static string OnFileTransferRequest(FileTransfer file)
+        {
+            Console.WriteLine(file.From + " wants to File transfer." + file.Name + " " + file.GetType());
+
+            // return null to refuse
+            return "allow";
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿using Artalk.Xmpp;
+using Artalk.Xmpp.Extensions;
+using Artalk.Xmpp.Im;
+using ArtalkTest;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace artalkTest
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            string hostname = "ott-eng-yipanw";
+            string username = "test1";
+            string password = "admin123";
+
+            ArtalkConnection artalkConnection = new(hostname, username, password);
+
+            bool isContinue = true;
+            bool isConnected = false;
+            List<RosterItem> contacts = new List<RosterItem>();
+
+
+            while (isContinue == true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Select: ");
+                if(!isConnected)
+                {
+                    Console.WriteLine("1 Connect to server(login)");
+                }
+                if(isConnected == true)
+                {
+                    Console.WriteLine("2 Get contacts");
+                    Console.WriteLine("3 Send Messages");
+                    Console.WriteLine("4 Set Activity");
+                    Console.WriteLine("5 Set Status");
+                    Console.WriteLine("6 Send a file");
+                    Console.WriteLine("7 get features");
+                }
+
+                Console.WriteLine("q Quit");
+
+                string command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "1":
+                        isConnected = artalkConnection.Connect();
+                        if(isConnected == true)
+                        {
+                            Console.WriteLine("Connected!!!");
+                            contacts = artalkConnection.getContacts();
+                        }
+                        break;
+                    case "2":
+                        contacts = artalkConnection.getContacts();
+                        Console.WriteLine("------ Contacts ------");
+                        contacts.ForEach(ro => Console.WriteLine("\n id: " + ro.Jid + "\n name: " + ro.Name + "\n group: " + String.Join(", ", ro.Groups)));
+                        break;
+                    case "3":
+                        Console.WriteLine("----- Sending Message ------");
+                        Jid firstContactId = contacts.FirstOrDefault().Jid;
+                        Console.WriteLine("jid to: " + firstContactId);
+                        string message = Console.ReadLine();
+                        bool isSent = artalkConnection.SendMessage(firstContactId, message);
+                        if (isSent == true) Console.WriteLine("message sent!");
+                        break;
+                    case "4":
+                        Console.WriteLine("---- Set activity to having breakfast -----");
+                        bool hasSet = artalkConnection.SetActivity(GeneralActivity.Eating, SpecificActivity.HavingBreakfast);
+                        if (hasSet == true) Console.WriteLine("activity set");
+
+                        break;
+                    case "5":
+                        Console.WriteLine("---- Set status to having a chat -----");
+                        bool hasChangedStatus = artalkConnection.SetStatus(Availability.Chat, "having a chat");
+                        if (hasChangedStatus == true) Console.WriteLine("status set");
+
+                        break;
+                    case "6":
+                        Console.WriteLine("---- Sending a file ----");
+                        string projectPath = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;
+                        string imagePath = Path.Combine(projectPath, "smile.png");
+
+                        Console.WriteLine("image path: " + imagePath);
+                        Jid sendFileTo = contacts.FirstOrDefault().Jid;
+                        Console.WriteLine("jid to: " + sendFileTo);
+                        artalkConnection.InitialFileTransfer(sendFileTo, imagePath);
+
+
+                        break;
+                    case "7":
+                        List<string> features = artalkConnection.getFeatures(artalkConnection.getOwnJid());
+
+                        features.ForEach(feature => Console.WriteLine(" feature>> " + feature));
+
+                        break;
+                    case "q":
+                        isContinue = false;
+                        break;
+
+                   default: 
+                        Console.WriteLine("Invalid");
+                        break;
+
+
+
+                }
+
+            }
+
+
+        }
+    }
+
+}
